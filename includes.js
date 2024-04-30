ae774c01c27958d0be42480330671302df72dad6
Tass Skoudros <1042292+tasssko@users.noreply.github.com>
2024-04-30 21:49:00 +0100

    removing some files that are not used

--- includes.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    axios: require('axios'),
-    http: require('http'),
-    https: require('https'),
-    lodash: require('lodash'),
-    querystring: require('querystring'),
-    Agent: require('agentkeepalive')
-}
